820ebd5903bb4398cc3e8da7864f007ec8541af6
Umair <sadiq.umair47@outlook.com>
2025-07-23 16:06:12 +0100

    commit

--- app/componetnts/ProfileOverview.jsx
+++ app/componetnts/ProfileOverview.jsx
@@ -3,7 +3,7 @@
 import {FaDownload} from "react-icons/fa";
 import {motion} from "framer-motion"; 
 import { FaGithub, FaLinkedin, FaMedium } from "react-icons/fa";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 const ProfileOverview = () => {
 
   const [isPaused, setIsPaused] = useState(false);
